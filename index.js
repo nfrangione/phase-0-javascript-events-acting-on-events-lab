21962f355e0fe4f18dee7f563d5b7f2a8ff178f2
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-25 13:14:06 -0800

    commit tree f5b7b1793441a257ace29723d0d72cb4cad8e862

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-    if (left < 400) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
 }
